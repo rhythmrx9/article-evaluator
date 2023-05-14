fb6ec1bc1dbac897f664616e1e3754ef88d37ad5
priyawart <priyawart@pop-os.localdomain>
2023-05-15 02:04:06 +0530

    changes made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
     hiddenFileInput.current.click();
   }
 
-  const handleFile = file =>{z
+  const handleFile = file =>{
     setloading(true)
     axios.post('/getgrades', file).then((res)=>{
       if (res.data){
@@ -36,18 +36,38 @@ function App() {
 
 
   return (
-    <div className='bg-white min-h-screen flex flex-col items-stretch justify-stretch'>
-      <div className = 'flex-[3] bg-gray-100 flex justify-center items-end'>
-        <h1 className='m-5 text-6xl font-bold'>Article Evaluator</h1>
-      </div>
-      <div className = 'flex flex-[4] bg-gray-100 items-center flex-col justify-start'>
-        <input type="file" name="file" onChange={handleChange} ref={hiddenFileInput} className='hidden'/>
-        <button onClick={handleClick} className="bg-green-100 hover:bg-green-200 shadow-lg h-16 w-36 rounded-lg m-4">
-          <h1 className='text-xl font-light'>{isloading?"Processing":"Upload File"}</h1>
-        </button>
+    <div className ="h-full">
+      <nav className="flex flex-[10] justify-end px-0">
+           
+    <ul className="flex justify-end">
+        
+       <a href="https://github.com/rhythmrx9/article-evaluator"target="_blank" ><img src={require('./git-logo-fotor-bg-remover-20230507121235.png')} className="w-24 "  alt=""/></a>
+    </ul>
+</nav>
+    <div className='bg-white  flex flex-col items-stretch justify-stretch'>
+    <div className="flex flex-col h-[20rem] justify-end items-center bg-white ">
+      <img className="w-24 h-24" src={require('./ullu.png')} alt=""/>
+      <h1 className="text-3xl text-gray-500" ><span className=" text-3xl  ">ARTICL</span><span className="text-5xl font-extrabold font-mono text-black">E</span>VALUATER</h1>
+      </div>   
+      <div className="flex flex-row h-[4rem] justify-center items-center bg-white">
+    
+<h1 className="text-gray-300 font-medium text-lg">"Better insights for a better world, through trusted article evaluation."</h1>
+
+</div>        
+      
+
+    <div className = 'flex flex-[4] bg-white items-center flex-col justify-start'>
+    <input type="file" name="file" onChange={handleChange} ref={hiddenFileInput} className='hidden'/>
+       
+    <button onClick={handleClick} className="bg-white rounded-lg px-12 py-3 border border-gray-400 hover:bg-slate-300 active:bg-slate-500 ">
+    <h1 className='text-xl font-light'>{isloading?"Processing":"Upload File"}</h1></button>
+    
+       
+            
+</div>
         <h1>{uploadedFile?uploadedFile.name:""}</h1>
       </div>
-      <div className='flex flex-row flex-[10] bg-gray-100  justify-between p-8'>
+      <div className='flex flex-row flex-[8] justify-between p-8'>
         <div className= 'flex-col flex  items-center'>
           <h1 className='text-4xl'>{grades?"cohesion":""}</h1>
           <h1 className='text-2xl font-light'>{grades?grades.cohesion:""} </h1>
